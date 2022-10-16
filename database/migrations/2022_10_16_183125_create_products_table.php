c226727ee9b4c08aeee04373acc86ee2011f8dbd
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-16 16:04:43 -0300

    Alterando Colunas e Mantendo dados

--- database/migrations/2022_10_16_183125_create_products_table.php
+++ database/migrations/2022_10_16_183125_create_products_table.php
@@ -16,15 +16,21 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             //Id do registro, autoincrement e primary-key
             $table->id();
+
             // Nome do produto (name) - string
             $table->string('name');
+
             //Observações do produto - text - null - (Obs)
             $table->text('obs')->nullable();
+
             // Código do produto - string - não pode repetir (code)
             $table->string('code')->unique();
+
             // Quantidade do produto - int - por default quantidade = 0
             $table->integer('quantity')->default(0);
+
             // Adicionar a quantidade minima
+            
 
             $table->timestamps();
         });
---
+++ database/migrations/2022_10_16_185921_alter_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Acessar a tabela 'products' e nela criar uma coluna 'min_quantity'.
+        Schema::table('products', function(Blueprint $table) {
+            $table->int('min_quantity')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
