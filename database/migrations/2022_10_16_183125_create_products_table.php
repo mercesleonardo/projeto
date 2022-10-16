bf0e097c6e6d4081ec301896407633b6e1df33ad
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-16 15:48:39 -0300

    Criando a Primeira Migration

--- database/migrations/2022_10_16_183125_create_products_table.php
+++ database/migrations/2022_10_16_183125_create_products_table.php
@@ -14,7 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
+            //Id do registro, autoincrement e primary-key
             $table->id();
+            // Nome do produto (name) - string
+            $table->string('name');
+            // Código do produto - string - não pode repetir (code)
+            $table->string('code')->unique();
+            // Quantidade do produto - int - por default quantidade = 0
+            $table->integer('quantity')->default(0);
+
             $table->timestamps();
         });
     }
